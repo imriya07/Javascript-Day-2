ca5bddcd22e4b9c3f8340d2c7e5d313723c47837
imriya07 <riya01022001@gmail.com>
2022-04-22 20:06:23 +0530

    update index

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+let fName = "John";//declared and initialized in the same line
+let fname;
+fname = "Hello";
+console.log(fname);
+fname = "Hi";
+console.log(fname);
+
+
+
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+
+
